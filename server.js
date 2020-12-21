ded56e7ae72446f749e0a78915e71982fc8cad59
Parminder <iparm1996@gmail.com>
2020-12-20 20:59:41 -0500

    Added CORS

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ dotenv.config();
 const app = express();
 connDB();
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use(morgan('dev'));
 app.use(express.json());
 //app.use((req, res, next) => setTimeout(next, 10000));
@@ -19,5 +28,8 @@ app.get('/', (req, res) => {
 
 app.use('/api/categories', categoriesRoutes);
 app.use('/api/products', productsRoutes);
-
-app.listen(8080, '0.0.0.0', console.log(`Express running @5000`));
+if (process.env.NODE_ENV === 'development') {
+  app.listen(5000, console.log(`Express running @5000`));
+} else {
+  app.listen(8080, '0.0.0.0', console.log(`Express running @8080`));
+}
